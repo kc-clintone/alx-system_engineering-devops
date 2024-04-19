4019fb27015c2b745841cb98b1d69db6ab58a36e
kc-clintone <kayseeclintone@gmail.com>
2024-04-19 19:47:06 +0000

    0x0A-configuration_management >>> task 001

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This will create a file called '0-create_a_file.pp' in the /tmp folder
 
 file {'/tmp/school':
-    ensure => 'file',
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0744',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
     content => 'I love Puppet',    
 }
