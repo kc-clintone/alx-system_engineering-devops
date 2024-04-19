7e13d6e3037c01c31bd1eaffec40618e843a8bfd
kc-clintone <kayseeclintone@gmail.com>
2024-04-19 21:30:25 +0000

    0x0B-ssh >> task 004

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# This puppet script creates a ssh config file
+file_line { 'Deactivate passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
