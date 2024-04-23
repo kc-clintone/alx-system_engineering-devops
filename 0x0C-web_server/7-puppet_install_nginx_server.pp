636641b55a972197c7d950920bc4805daeabec93
kc-clintone <kayseeclintone@gmail.com>
2024-04-23 22:12:10 +0000

    0x0C-web_server >>> task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_server.pp
@@ -0,0 +1,25 @@
+# Install nginx using puppet
+
+package {'get latest nginx version':
+  ensure => 'present',
+}
+
+exec {'install nginx':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello world message':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
