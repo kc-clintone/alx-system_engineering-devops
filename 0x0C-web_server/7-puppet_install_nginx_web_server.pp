2eb451d0b11d23c56e0900d8511c22310c9ad216
kc-clintone <kayseeclintone@gmail.com>
2024-04-24 20:17:30 +0000

    0x0C-web_server >>> task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,13 +10,11 @@ exec {'install nginx':
 
 }
 
-exec {'Hello world message':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301
-https:\/\/www.github.com/kc-clintone\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.github.com/kc-clintone\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
