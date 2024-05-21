9465fe9f93bb0c16cf8fb26a662a37df630cc0ff
kc-clintone <kayseeclintone@gmailcom>
2024-05-21 12:01:45 +0000

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Use puppet to install flask from pip3
 # version must be 2.1.0
 
-exec {'Install flask':
-    command => '/usr/bin/pip3 install flask==2.1.0'
+# Install Flask v2 and Werkzeug globally
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep 2.1.0',
 }
