5c50dc8173c440b4c3f97a27027c38b31f9aace7
kc-clintone <kayseeclintone@gmailcom>
2024-05-22 21:42:00 +0000

    0x15-api >>> task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+"""
+returns info about employee TODO progress
+Implemented using recursion
+"""
+import re
+import requests
+import sys
+
+
+ENDPOINT = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(ENDPOINT, id)).json()
+            todos_res = requests.get('{}/todos'.format(ENDPOINT)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
