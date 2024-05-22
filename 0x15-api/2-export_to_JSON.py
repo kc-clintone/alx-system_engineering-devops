43dab1cb64efa39b66840f0b0ec182c84a878480
kc-clintone <kayseeclintone@gmailcom>
2024-05-22 21:55:10 +0000

    0x15-api >>> task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+gathers data from API and exports it to JSON file
+Implemented using recursion
+"""
+import re
+import requests
+import json
+import sys
+
+
+ENDPOINT = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            usr_id = int(sys.argv[1])
+            users = requests.get('{}/users/{}'.format(ENDPOINT, usr_id)).json()
+            todos_obj = requests.get('{}/todos'.format(ENDPOINT)).json()
+            usr_name = users.get('username')
+            todos = list(filter(lambda x: x.get('userId') == usr_id, todos_obj))
+            with open("{}.json".format(usr_id), 'w') as json_file:
+                results = list(map(
+                    lambda x: {
+                        "task": x.get("title"),
+                        "completed": x.get("completed"),
+                        "username": usr_name
+                    },
+                    todos
+                ))
+                results = {
+                    "{}".format(id): results
+                }
+                json.dump(results, json_file)
