03e6af7d3e24bdd35036cf74a731dbe0e6b9b487
kc-clintone <kayseeclintone@gmailcom>
2024-05-24 11:17:02 +0000

    corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,47 @@
-# Install nginx using puppet
+# Automating using puppet
 
-package {'get latest nginx version':
-  ensure => 'present',
+# Ensure Nginx package is installed
+package { 'nginx':
+  ensure => installed,
 }
 
-exec {'install nginx':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-  provider => shell,
-
+# Ensure the root HTML file is present with the required content
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  require => Package['nginx'],
 }
 
-file {'/var/www/html/index.nginx-debian.html':
-    content => 'Hello World!',
+# Use exec resource to add the rewrite rule if it doesn't already exist
+exec { 'add_nginx_rewrite_rule':
+  command => 'sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.github.com/kc-clintone permanent;" /etc/nginx/sites-available/default',
+  unless  => 'grep "rewrite ^/redirect_me https://www.github.com/kc-clintone permanent;" /etc/nginx/sites-available/default',
+  path    => ['/bin', '/usr/bin'],
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.github.com/kc-clintone\/;\\n\\t}/" /etc/nginx/sites-available/default':
-  provider => shell,
+# Use exec resource to configure Nginx settings
+exec { 'configure_nginx':
+  command => 'sed -i "/^\\s*root /d" /etc/nginx/sites-available/default; \
+              sed -i "/^\\s*index /d" /etc/nginx/sites-available/default; \
+              sed -i "/server_name _;/a \\    root /var/www/html;" /etc/nginx/sites-available/default; \
+              sed -i "/server_name _;/a \\    index index.html index.htm;" /etc/nginx/sites-available/default; \
+              sed -i "/try_files \\$uri \\$uri\\/ =404;/d" /etc/nginx/sites-available/default; \
+              sed -i "/location \\/ {/a \\        try_files \\$uri \\$uri\\/ =404;" /etc/nginx/sites-available/default',
+  unless  => 'grep "rewrite ^/redirect_me https://www.github.com/kc-clintone permanent;" /etc/nginx/sites-available/default',
+  path    => ['/bin', '/usr/bin'],
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
-exec {'restart nginx':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+# Ensure the Nginx service is running and enabled to start at boot
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => Exec['add_nginx_rewrite_rule', 'configure_nginx'],
 }
