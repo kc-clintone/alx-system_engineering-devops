5d3621eb16dafbb09f89bf6e116ddaa63a9f3229
kc-clintone <kayseeclintone@gmailcom>
2024-05-25 08:20:18 +0000

    corrections

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,23 +5,11 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Define custom HTTP header value as the hostname of the server
-$server_hostname = $facts['hostname']
-
-# Configure Nginx to add custom HTTP header response
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        # Add custom HTTP header response
-        add_header X-Served-By
+exec { 'configure_nginx':
+  command  => '/usr/bin/apt-get -y update && \
+               /usr/bin/apt-get -y install nginx && \
+               /bin/sed -i "/listen 80 default_server;/a add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default && \
+               /usr/sbin/service nginx restart',
+  provider => shell,
+}
 
