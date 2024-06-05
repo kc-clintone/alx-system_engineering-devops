be14ae9a5ebfa400c1a889fb006da288638bb1d3
kc-clintone <kayseeclintone@gmail.com>
2024-06-05 07:40:04 +0000

    0x16-api_advanced >>> task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,7 @@ def number_of_subscribers(subreddit):
     }
 
     try:
-        response = requests.get(url, headers=headers, \
+        response = requests.get(url, headers=headers,
                                 allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
