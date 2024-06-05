c205b4c44c771a45e13a913c218e0f78be733f6b
kc-clintone <kayseeclintone@gmail.com>
2024-06-05 07:59:52 +0000

    0x16-api_advanced >>> task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': '0x16-api_advanced:project:
+        'User-Agent': 'python:subreddit.subscriber.counter: \
          v1.0 (by /u/Particular-Cattle673)'
     }
 
