b73d815107ead5c9825f8869ab4ac4f44338f8f0
kc-clintone <kayseeclintone@gmail.com>
2024-06-05 08:07:09 +0000

    0x16-api_advanced >>> task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Count top ten posts
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Returns top ten posts
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {
+        'User-Agent': 'python:subreddit.hot.posts:v1.0 (by /u/yourusername)'
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
