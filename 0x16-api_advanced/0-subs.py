4d79ac25e2d6436d7cc0e5049ab4d180912396c7
kc-clintone <kayseeclintone@gmail.com>
2024-06-05 07:01:48 +0000

    0x16-api_advanced >>> task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,11 +12,13 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)'
+        'User-Agent': 'python:subreddit.subscriber.counter:\
+         v1.0 (by /u/yourusername)'
     }
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers,\
+        allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
