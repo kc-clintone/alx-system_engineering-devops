b34ae44917b9270edbf9197ff198a44548d663d9
kc-clintone <kayseeclintone@gmail.com>
2024-06-06 15:07:43 +0000

    0x17-web_stack_debugging_3 >>> task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# What's wrong with appache? Let's fix it
+exec { 'fix-appache':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
