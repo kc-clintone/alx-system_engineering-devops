088f9a165840497f5b30e45d8a31d136aa963a86
kc-clintone <kayseeclintone@gmail.com>
2024-06-06 14:29:53 +0000

    0x16-api_advanced >>> task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,48 +1,56 @@
 #!/usr/bin/python3
 """
-Count
+This function queries the Reddit API, returns the number of
+subscribers for a given subreddit.
 """
 
-from collections import Counter
-import re
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counter=None):
+def count_words(subreddit, word_list, after=None, counts=None):
     """
-    Counts top hot posts
+    This function queries the Reddit API, returns the number of
+    subscribers for a given subreddit.
     """
-    if counter is None:
-        counter = Counter()
+    if counts is None:
+        counts = {}
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'python:subreddit.keyword.counter: \
-               v1.0 (by /u/Particular-Cattle673)'}
+    headers = {
+        'User-Agent': 'python:subreddit.word.counter:v1.0 \
+                       (by /u/Particular-Cattle673)'
+    }
     params = {'limit': 100}
     if after:
         params['after'] = after
 
     try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
         if response.status_code != 200:
             return
+
         data = response.json()
         posts = data['data']['children']
         for post in posts:
-            title = post['data']['title']
-            words_in_title = re.findall(r'\b\w+\b', title.lower())
+            title = post['data']['title'].lower()
             for word in word_list:
-                counter[word.lower
-                        ()] += words_in_title.count(word.lower())
+                word_lower = word.lower()
+                word_count = title.split().count(word_lower)
+                if word_count > 0:
+                    if word_lower in counts:
+                        counts[word_lower] += word_count
+                    else:
+                        counts[word_lower] = word_count
 
-        after = data['data'].get('after')
+        after = data['data']['after']
         if after:
-            count_words(subreddit, word_list, after, counter)
+            count_words(subreddit, word_list, after, counts)
         else:
-            sorted_counts = sorted(counter.items(),
+            sorted_counts = sorted(counts.items(),
                                    key=lambda x: (-x[1], x[0]))
             for word, count in sorted_counts:
-                if count > 0:
-                    print(f"{word}: {count}")
+                print(f"{word}: {count}")
+
     except requests.RequestException:
         return
