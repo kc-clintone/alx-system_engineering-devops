eeae8fe06ebc286cdbd507bc14bce250db703b93
kc-clintone <kayseeclintone@gmail.com>
2024-06-12 21:23:25 +0000

    0x1B-web_stack_debugging_4 >>> task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Let's fixe an nginx server to handle a bunch load of requests
+exec { 'scale-ngix':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
