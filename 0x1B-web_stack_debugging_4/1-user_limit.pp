c40d1ef925c17382b5ce78c05d6c1e9dde1f8c77
kc-clintone <kayseeclintone@gmail.com>
2024-06-12 21:25:27 +0000

    0x1B-web_stack_debugging_4 >>> task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# Changes OS config
+exec { 'mod-os-config':
+  command => "bash -c \"sed -iE 's/^holberton hard nofile \
+5/holberton hard nofile 88888/' /etc/security/limits.conf; \
+sed -iE 's/^holberton soft nofile \
+4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
